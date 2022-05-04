79ff74b0f98fdef816c02885fc1f8c07fd2f1b8d
Jhon Arzu-Gil <JhonGil@ibm.com>
2022-05-03 20:47:30 -0500

    Part 2

---
+++ app/src/main/java/com/arzugil/chatsnap/SnapsActivity.kt
@@ -0,0 +1,11 @@
+package com.arzugil.chatsnap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SnapsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_snaps)
+    }
+}
